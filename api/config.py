bfd840f02ad4ff98f71da83c46a2fe0679359369
thedaebu <min.kim163@gmail.com>
2023-09-09 21:20:02 -0400

    add config file

---
+++ api/config.py
@@ -0,0 +1,29 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = '61ab1e237cf349a388da36b266953988'
+    SQLALCHEMY_DATABASE_URI = "postgresql:///descart_dev"
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
