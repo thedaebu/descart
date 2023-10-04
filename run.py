ea6ba0bac55f3214d622e8e3d3e85db7496776ef
thedaebu <min.kim163@gmail.com>
2023-10-04 14:39:00 -0400

    add port atribute for running app

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ migrate = Migrate(app, db)
 # can be used when running file using python command
 # automatically updates display when edits are made
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
