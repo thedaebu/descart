2478ced15379907b30c0417c5a9278939dcda763
thedaebu <min.kim163@gmail.com>
2023-10-04 14:42:45 -0400

    setup routes file

--- api/carts/routes.py
+++ api/carts/routes.py
@@ -3,13 +3,12 @@ from api.models import Cart
 
 carts = Blueprint('carts', __name__)
 
-
 # Keep before request in consideration
 # @main.before_request
 # def before():
 #     print("This is executed BEFORE each request.")
 
-@carts.route('/cart/<int:cart_id>', methods=['GET'])
+@carts.route('/api/cart/<int:cart_id>', methods=['GET'])
 def cart_show(cart_id):
     cart = Cart.query.get_or_404(cart_id)
     return jsonify(cart.serialize())
