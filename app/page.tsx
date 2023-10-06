0fd875777882710df5bbcd7a3cef2492419a53b3
thedaebu <min.kim163@gmail.com>
2023-10-06 15:45:40 -0400

    get nextjs to show data from fetched api data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+'use client'
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 function Page() {
+    const [carts, setCarts] = useState<Object>({});
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/carts')
+            .then(res => res.json())
+            .then(data => setCarts(() => data));
+    }, []);
+
     return (
         <>
             <h1>Hello, Next.js!s</h1>
-            <Link href={"/cart"}>cart</Link>
+            {Object.keys(carts).length > 0 && (
+                <>
+                    {Object.values(carts)[0].address}
+                </>
+            )}
         </>
     );
 }
