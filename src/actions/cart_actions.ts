a630f61b9dae2b5cb2243c0ef7173ea2bfd24281
thedaebu <min.kim163@gmail.com>
2023-10-17 15:26:02 -0400

    add search parameter to fetchCarts

--- src/actions/cart_actions.ts
+++ src/actions/cart_actions.ts
@@ -1,5 +1,6 @@
 import * as CartAPIUtil from "../util/api/cart_api_util";
 import { AnyAction, Dispatch } from "@reduxjs/toolkit";
+import { AxiosResponse } from "axios";
 
 export const RECEIVE_CARTS: string = "RECEIVE_CARTS";
 
@@ -8,7 +9,7 @@ const receiveCarts: Function = ({ data }: { data: any; }) => ({
     type: RECEIVE_CARTS
 });
 
-export const fetchCarts: Function = () => (dispatch: Dispatch<AnyAction>) => (
-    CartAPIUtil.fetchCarts()
-        .then((carts: any) => dispatch(receiveCarts(carts)))
+export const fetchCarts: Function = (search: string) => (dispatch: Dispatch<AnyAction>) => (
+    CartAPIUtil.fetchCarts(search)
+        .then((carts: AxiosResponse) => dispatch(receiveCarts(carts)))
 );
