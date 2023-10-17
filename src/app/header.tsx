08c45b3b527b87dfc86bfbca946cf8fe01ba1604
thedaebu <min.kim163@gmail.com>
2023-10-17 13:49:57 -0400

    add header placeholder

---
+++ src/app/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <h1 className='header'>
+            <p className='header-text'>
+                Descart Header
+            </p>
+        </h1>
+    )
+}
+
+export default Header;
