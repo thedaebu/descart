f4513b28ad1cf0ba8b3c63b124d45feba0591423
thedaebu <min.kim163@gmail.com>
2023-10-17 14:06:51 -0400

    add cart list column

---
+++ src/app/cart-list-column.tsx
@@ -0,0 +1,9 @@
+function CartListColumn() {
+    return (
+        <section className='cart-list-column'>
+            cart list Column
+        </section>
+    );
+}
+
+export default CartListColumn;
