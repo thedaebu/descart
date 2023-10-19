abb5ea5918909c40c99ce19adfa5c8f9c4f145a8
thedaebu <min.kim163@gmail.com>
2023-10-19 14:40:43 -0400

    add debounce functionality for earch feature

--- src/app/search-column.tsx
+++ src/app/search-column.tsx
@@ -1,18 +1,29 @@
 'use client'
-import { ChangeEvent, SetStateAction, useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import * as cartActions from "../actions/cart_actions";
+import useDebounce from "@/hooks/debounce_hook";
 
-function SearchColumn({ handleSearchParams }: { handleSearchParams: Function; }) {
+
+function SearchColumn() {
     const [searchField, setSearchField] = useState<string>("");
 
-    function handleSearchField(e: ChangeEvent<HTMLInputElement>) {
-        handleSearchParams(e.currentTarget.value);
-        setSearchField(e.currentTarget.value);
-    }
+    const dispatch = useDispatch();
+    const fetchCarts = (search: string) => dispatch(cartActions.fetchCarts(search));
+
+    const debouncedSearch = useDebounce(searchField)
+
+    useEffect(() => {
+        if (debouncedSearch) {
+            fetchCarts(debouncedSearch);
+        }
+    }, [debouncedSearch]);
 
     return (
         <section className='search-column'>
             <input
-                onChange={handleSearchField}
+                className='search-column__input'
+                onChange={(e) => setSearchField(e.target.value)}
                 placeholder='Search'
                 type='text'
                 value={searchField}
