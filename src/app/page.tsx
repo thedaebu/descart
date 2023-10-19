797571a3dce6c52ae90c4b3d5e88403fd3459459
thedaebu <min.kim163@gmail.com>
2023-10-19 14:39:35 -0400

    import Columns component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,12 @@
 'use client'
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Link from "next/link";
-import * as cartActions from "../actions/cart_actions";
-import { Cart } from "@/my_types";
 import Header from "./header";
-import CartShowColumn from "./cart-show-column";
-import SearchColumn from "./search-column";
-import CartListColumn from "./cart-list-column";
+import Columns from "./columns";
 
 function Page() {
-    const carts: { [key: number]: Cart; } = useSelector((state: any) => state.entities.carts);
-
-    const [searchParams, setSearchParams] = useState<string>("");
-
-    const dispatch = useDispatch();
-    const fetchCarts = (search: string) => dispatch(cartActions.fetchCarts(search));
-
-    useEffect(() => {
-        fetchCarts("Portland");
-    }, []);
-
-    function handleSearchParams(string: string) {
-        setSearchParams(() => string);
-    }
-
     return (
         <>
-            {<Header />}
-            <section className='columns'>
-                <SearchColumn handleSearchParams={handleSearchParams} />
-                <CartListColumn />
-                <CartShowColumn carts={carts} />
-            </section>
+            <Header />
+            <Columns />
         </>
     );
 }
