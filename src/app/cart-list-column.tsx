0c411d87132d1a5739cd05acb7de3683dffc671a
thedaebu <min.kim163@gmail.com>
2023-10-26 18:47:30 -0400

    add cart list item components for each cart

--- src/app/cart-list-column.tsx
+++ src/app/cart-list-column.tsx
@@ -1,7 +1,30 @@
-function CartListColumn() {
+import { Cart } from "@/my_types";
+import CartListItem from "./cart-list-item";
+
+type Props = {
+    carts: { [key: number]: Cart; };
+    currentCart: number;
+    handleCurrentCart: Function;
+};
+
+function CartListColumn(props: Props) {
+    const { carts, currentCart, handleCurrentCart } = props;
+
     return (
         <section className='cart-list-column'>
-            cart list Column
+            <h2 className='cart-list-column__header'>Cart List</h2>
+            {Object.keys(carts).length > 0 && (
+                <ul>
+                    {Object.values(carts).map(cart => (
+                        <CartListItem
+                            cart={cart}
+                            currentCart={currentCart}
+                            handleCurrentCart={handleCurrentCart}
+                            key={cart.id}
+                        />
+                    ))}
+                </ul>
+            )}
         </section>
     );
 }
