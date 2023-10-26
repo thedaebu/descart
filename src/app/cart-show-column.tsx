7a83d322b3f2a020cad36ab9e503c325616fc78f
thedaebu <min.kim163@gmail.com>
2023-10-26 18:49:15 -0400

    add display functions for current cart

--- src/app/cart-show-column.tsx
+++ src/app/cart-show-column.tsx
@@ -1,19 +1,39 @@
 import { Cart } from "@/my_types";
+import { useSelector } from "react-redux";
 
-function CartShowColumn({ carts }: { carts: { [key: number]: Cart; }}) {
+function CartShowColumn({ currentCart }: { currentCart: number; }) {
+    const cart = useSelector((state: any) => state.entities.carts[currentCart]);
+
+    function cartDisplay() {
+        const { address, areaCode, city, id, name, phoneNumber, state, status, website, zipCode } = cart;
+
+        return (
+            <>
+                <h2>{name}</h2>
+                {addressDisplay(address, city, state, zipCode)}
+                <p>{phoneDisplay(areaCode, phoneNumber)}</p>
+                {website && <p>{website}</p>}
+            </>
+        )
+    }
+
+    function addressDisplay(address: string, city: string, state: string, zipCode: string) {
+        return (
+            <section>
+                <p>{address}</p>
+                <p>{`${city}, ${state} ${zipCode}`}</p>
+            </section>
+        );
+    }
+
+    function phoneDisplay(areaCode: string, phoneNumber: string) {
+        return `(${areaCode}) ${phoneNumber.slice(0,3)}-${phoneNumber.slice(3)}`;
+    }
+
     return (
         <section className='cart-show-column'>
             Cart show column
-            {Object.keys(carts).length > 0 && (
-                <ul>
-                    {Object.values(carts).map(cart => (
-                        <li>
-                            <p>{cart.address}</p>
-                            <p>{cart.name}</p>
-                        </li>
-                    ))}
-                </ul>
-            )}
+            {currentCart >= 0 && cartDisplay()}
         </section>
     );
 }
