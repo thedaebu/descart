fc5093b2e9376f3faf4a3ab5ffd3eebc1d92bb85
thedaebu <min.kim163@gmail.com>
2023-11-04 14:24:26 -0400

    update cart model with instagram column

--- api/carts/seeds.py
+++ api/carts/seeds.py
@@ -1,10 +1,12 @@
 from api.models import Cart
 
-pelmeniPelmeni = Cart(address='1080 SE Madison St', areaCode='503',
-                      city='Portland', name='Pelmeni Pelmeni',
-                      phoneNumber='9084570', state='OR', status=True,
-                      website='', zipCode='97214')
-baonKaonan = Cart(address='1080 SE Madison St', areaCode='503',
-                  city='Portland', name='Dae Bap', phoneNumber='9084571',
-                  state='OR', status=True, website='', zipCode='97214')
+pelmeniPelmeni = Cart(address='1080 SE Madison St', area_code='503',
+                      city='Portland', instagram='', name='Pelmeni Pelmeni',
+                      phone_number='9084570', state='OR', status=True,
+                      website='', zip_code='97214')
+baonKaonan = Cart(address='1080 SE Madison St', area_code='503',
+                  city='Portland',
+                  instagram='https://www.instagram.com/pelmenipdx/',
+                  name='Dae Bap', phone_number='9084571',
+                  state='OR', status=True, website='', zip_code='97214')
 cartSeeds = [baonKaonan, pelmeniPelmeni]
