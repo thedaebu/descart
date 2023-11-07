029ce6ef7a2067fa0037fe6c8f3ebbcb600b4f77
thedaebu <min.kim163@gmail.com>
2023-11-07 13:27:42 -0500

    edit instagram data

--- api/carts/seeds.py
+++ api/carts/seeds.py
@@ -1,12 +1,12 @@
 from api.models import Cart
 
-pelmeniPelmeni = Cart(address='1080 SE Madison St', area_code='503',
-                      city='Portland', instagram='', name='Pelmeni Pelmeni',
-                      phone_number='9084570', state='OR', status=True,
-                      website='', zip_code='97214')
 baonKaonan = Cart(address='1080 SE Madison St', area_code='503',
                   city='Portland',
-                  instagram='https://www.instagram.com/pelmenipdx/',
+                  instagram='',
                   name='Dae Bap', phone_number='9084571',
                   state='OR', status=True, website='', zip_code='97214')
+pelmeniPelmeni = Cart(address='1080 SE Madison St', area_code='503',
+                      city='Portland', instagram='pelmenipdx',
+                      name='Pelmeni Pelmeni', phone_number='9084570',
+                      state='OR', status=True, website='', zip_code='97214')
 cartSeeds = [baonKaonan, pelmeniPelmeni]
