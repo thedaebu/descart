fdd5692bed22e41042f5bee1101c00b032603294
thedaebu <min.kim163@gmail.com>
2023-11-10 13:54:33 -0500

    fix info

--- api/carts/seeds.py
+++ api/carts/seeds.py
@@ -1,11 +1,12 @@
 from api.models import Cart
 
 baonKaonan = Cart(address='1080 SE Madison St', area_code='503',
-                  city='Portland', google_id='ChIJleDwNjGhlVQR9ACRM6bbDAo',
+                  city='Portland', google_id='',
                   instagram='', name='Dae Bap', phone_number='9084571',
                   state='OR', status=True, website='', zip_code='97214')
 pelmeniPelmeni = Cart(address='1080 SE Madison St', area_code='503',
-                      city='Portland', google_id='', instagram='pelmenipdx',
-                      name='Pelmeni Pelmeni', phone_number='9084570',
-                      state='OR', status=True, website='', zip_code='97214')
+                      city='Portland', google_id='ChIJleDwNjGhlVQR9ACRM6bbDAo',
+                      instagram='pelmenipdx', name='Pelmeni Pelmeni',
+                      phone_number='9084570', state='OR', status=True,
+                      website='', zip_code='97214')
 cartSeeds = [baonKaonan, pelmeniPelmeni]
